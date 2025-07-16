202389f5fbb36aa341dbf4db57f279bd92867ccc
6ayoob <tayoob632@gmail.com>
2025-07-17 01:40:48 +0300

    Update set_webhook.py

--- set_webhook.py
+++ set_webhook.py
@@ -4,7 +4,7 @@ import requests
 TOKEN = "7708106993:AAHpd2Ceo7IQaeAHWif3uUntKq7dJCPC9K0"
 
 # رابط مشروعك على Render (غيره إلى رابط مشروعك الحقيقي)
-WEBHOOK_URL = f"https://your-app-name.onrender.com/{TOKEN}"
+WEBHOOK_URL = f"https://kucoin-auto-trader.onrender.com/{TOKEN}"
 
 # طلب تعيين Webhook
 response = requests.get(f"https://api.telegram.org/bot{TOKEN}/setWebhook?url={WEBHOOK_URL}")
