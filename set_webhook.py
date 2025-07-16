bc9bf56a3857d694272fb51128c0e95813ec3820
6ayoob <tayoob632@gmail.com>
2025-07-17 01:37:11 +0300

    Create set_webhook.py

---
+++ set_webhook.py
@@ -0,0 +1,13 @@
+import requests
+
+# توكن البوت
+TOKEN = "7708106993:AAHpd2Ceo7IQaeAHWif3uUntKq7dJCPC9K0"
+
+# رابط مشروعك على Render (غيره إلى رابط مشروعك الحقيقي)
+WEBHOOK_URL = f"https://your-app-name.onrender.com/{TOKEN}"
+
+# طلب تعيين Webhook
+response = requests.get(f"https://api.telegram.org/bot{TOKEN}/setWebhook?url={WEBHOOK_URL}")
+
+# طباعة النتيجة
+print(response.json())
