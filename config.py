0bce876902e23fc68a5a4398a6ae5dec9834988c
6ayoob <tayoob632@gmail.com>
2025-07-17 01:47:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,17 @@
-# config.py
 
+# توكن بوت تليجرام
 TELEGRAM_BOT_TOKEN = "7863509137:AAHBuRbtzMAOM_yBbVZASfx-oORubvQYxY8"
 
 # القائمة المسموح لها بالتواصل مع البوت
 ALLOWED_CHAT_IDS = [
-    7863509137,   # حسابك الرئيسي
-    658712542     # جهازك أو حسابك الآخر
+    7863509137,  # حسابك الرئيسي
+    658712542    # جهازك أو حسابك الآخر
 ]
+
+# إعدادات KuCoin
+KUCOIN_API_KEY = "6877ac1dc714e80001eeac72"
+KUCOIN_API_SECRET = "1672504f-df06-4c84-9702-63daeefd08c5"
+KUCOIN_API_PASSPHRASE = "700110"  # كلمة المرور التداولية الخاصة بك
+
+# إعداد نسبة وقف الخسارة
+STOP_LOSS_PERCENT = 5  # يمكنك تعديلها حسب استراتيجيتك
